a61930a21606f7b998d2ebc4081c5b22349f7adc
Kaj Schmidt <kaj.schmidt@umea.nti.se>
2017-08-25 15:35:06 +0200

    test

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ function buildPage(callBack) {
           $("body").append("<article id='page'><section><div id='sender_name'>"+ obj["sender"] +"</div><div id='sender_message'>"+ obj["message"] +"</div></section></article>");
           $("#page").css("background-image", "url('"+ obj["image"] +"')");
           $("#page").delay(2000).fadeIn('slow', function(){
-            $("#page section").delay(500).css("display", "flex").hide().slideDown('slow');
+            //$("#page section").delay(500).css("display", "flex").hide().slideDown('slow');
           });
           setTimeout(function(){ buildPage(obj["image"]); }, 3000);
         });
